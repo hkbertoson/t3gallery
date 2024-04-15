686c949548e8d724869e5f9c5d111af6042ed776
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-15 12:41:52 -0500

    Linking db to ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`font-sans ${inter.variable}  flex flex-col gap-4`}>
         <TopNav />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
-const mockUrls = [
-  "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-  "https://utfs.io/f/93fe9634-fea2-4314-956b-7ae7f700ae5b-qa0vsq.png",
-  "https://utfs.io/f/4709ed08-e620-4c42-9d65-94af53585014-lf3twj.png",
-  "https://utfs.io/f/c494ae05-dd1a-459a-b09d-bbe37a140853-qikpqu.1.png",
-];
+import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
-export default function HomePage() {
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {images.map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
