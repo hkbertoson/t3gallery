37dc9e69e1991f42213cfbb8ad1d5b32127aac3f
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-15 12:26:06 -0500

    Added force-dynamic and other VS Code Settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 const mockUrls = [
-  "https://utfs.io/f/ef638675-3f3a-479d-b3bb-c91b1356fad5-ajf943.jpg",
-  "https://utfs.io/f/ddb2549f-153c-443d-b1a2-4b16aad0c1f3-47z1ax.jpg",
-  "https://utfs.io/f/fda05283-85e4-48be-9f6f-360f40b72965-m4u0s4.jpg",
-  "https://utfs.io/f/4fe46543-3648-43ac-a41a-b5ccdc06a3af-g5fngt.jpg",
+  "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
+  "https://utfs.io/f/93fe9634-fea2-4314-956b-7ae7f700ae5b-qa0vsq.png",
+  "https://utfs.io/f/4709ed08-e620-4c42-9d65-94af53585014-lf3twj.png",
+  "https://utfs.io/f/c494ae05-dd1a-459a-b09d-bbe37a140853-qikpqu.1.png",
 ];
+
+export const dynamic = "force-dynamic";
 
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
@@ -14,8 +16,8 @@ export default function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
