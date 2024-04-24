44b5d5d3334d5f3901e19290b6221d7ff616fb72
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-24 12:38:04 -0500

    Adding padding and updated other things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,8 +43,10 @@ export default function RootLayout({
           className={`font-sans ${inter.variable}  flex flex-col gap-4`}
           suppressHydrationWarning={true}
         >
-          <TopNav />
-          {children}
+          <div className="grid h-screen grid-rows-[auto,1fr]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,8 +3,12 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <div>
-      <img src={image.url} className="w-96" />
+    <div className="flex w-full ">
+      <img src={image.url} className="w-96 min-w-0 object-contain" />
+
+      <div className="flex w-48 flex-col">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
     </div>
   );
 }
