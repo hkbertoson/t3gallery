909a3825cdf91d4146697865e4dc4b7222c90673
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-24 17:34:54 -0500

    Delete Button

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,17 +1,15 @@
 import { Modal } from "./modal";
-import FullPageImageView from "~/components/full-image-page";
+import { FullPageImageView } from "~/common/full-image-page-view";
 
 export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = Number(photoId);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid Photo ID");
   return (
     <div>
       <Modal>
-        <FullPageImageView id={idAsNumber} />
+        <FullPageImageView photoId={photoId} />
       </Modal>
     </div>
   );
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,15 +1,13 @@
-import FullPageImageView from "~/components/full-image-page";
+import { FullPageImageView } from "~/common/full-image-page-view";
 
 export default function PhotoPage({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = Number(photoId);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid Photo ID");
   return (
     <div>
-      <FullPageImageView id={idAsNumber} />
+      <FullPageImageView photoId={photoId} />
     </div>
   );
 }
